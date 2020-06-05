4d0f9e6ac5eaee6b5bae49b8ae899ac77a772c46
Ivan Chien <qyc027@gmail.com>
2020-06-05 16:25:19 +0800

    Delete other repo copies

--- NASM/integer_extend_test.c
+++
@@ -1,7 +0,0 @@
-int main(void) {
-    volatile unsigned char uchar = 0xff;
-    volatile signed   char schar = 0xff;
-    volatile int a = (int) uchar;
-    volatile int b = (int) schar;
-    return 0;
-}
--- Scheme/readme.md
+++
@@ -1,3 +0,0 @@
-# Scheme Testing - Scheme代码测试
-一个新鲜的repos, 主要存放看sicp时候自己手搓的或者是抄写的代码.   
-语言这边使用了方言Scheme, 所以叫Scheme Testing, 而不叫Lisp Testing.  
