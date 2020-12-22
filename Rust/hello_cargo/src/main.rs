837cac029cb954a2833194c3fe09476cbaceb608
Ivan Chien <qyc027@gmail.com>
2020-12-22 21:19:24 +0800

    auto-commit.sh: 2020/12/22 21时19分24秒

--- Rust/hello_cargo/src/main.rs
+++ Rust/hello_cargo/src/main.rs
@@ -1,3 +1,53 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {
+            x,
+            y,
+        }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+
 fn main() {
-    println!("Hello, cargo!");
+    let number_list = vec![10, 20, 30 ,40 ,50];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let pair = Pair::new(1, 2);
+    pair.cmp_display();
 }
