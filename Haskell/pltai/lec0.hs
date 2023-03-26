8474a5a763c3520943f7652821f138f424d73ec7
Ivan Chien <qyc027@gmail.com>
2023-03-26 10:17:11 +0800

    Finish PLTAI lec0

--- Haskell/pltai/lec0.hs
+++ Haskell/pltai/lec0.hs
@@ -1,6 +1,4 @@
 import Data.Maybe
-import Data.Fixed (E1)
-import Distribution.Simple.Utils (xargs)
 
 data Expr0
   = E0Cst Int
@@ -91,6 +89,50 @@ compileE1EN (E1Let name val expr) names = ENLet (compileE1EN val names) (compile
 
 test6 = evalExprNameless (compileE1EN expr1Instance []) []
 
+-- hw3-1
+data Instr1
+    = I1Cst Int
+    | I1Add
+    | I1Mul
+    | I1Var Int
+    | I1Swap
+    | I1Pop
+type Instr1s = [Instr1]
+type I1Stack = [Int]
+
+evalInstr1 :: Instr1s -> I1Stack -> Int
+evalInstr1 (I1Cst x : rest) s = evalInstr1 rest (x : s)
+evalInstr1 (I1Add : rest) (a : b : s) = evalInstr1 rest (a + b : s)
+evalInstr1 (I1Mul : rest) (a : b : s) = evalInstr1 rest (a * b : s)
+evalInstr1 (I1Var i : rest) s = evalInstr1 rest (s !! i : s)
+evalInstr1 (I1Swap : rest) (a : b : s) = evalInstr1 rest (b : a : s)
+evalInstr1 (I1Pop : rest) (_ : s) = evalInstr1 rest s
+evalInstr1 [] [top] = top
+evalInstr1 _ _ = undefined
+
+instr1Instance = [I1Cst 2, I1Var 0, I1Var 1, I1Add, I1Swap, I1Pop]
+test7 :: Int
+test7 = evalInstr1 instr1Instance []
+
+-- hw3-2
+data VarType = VOperand | VVar
+
+mapsEnvar2I1var :: [VarType] -> Int -> Int
+mapsEnvar2I1var vts i = aux vts i 0
+    where aux :: [VarType] -> Int -> Int -> Int
+          aux (VOperand : rest) i ii = aux rest i ii + 1
+          aux (VVar : rest) 0 ii = ii
+          aux (VVar : rest) i ii = aux rest (i - 1) (ii + 1)
+
+compileNEI1 :: ExprNameless -> [VarType] -> I1Stack -> Instr1s
+compileNEI1 (ENCst x) vts s = [I1Cst x]
+compileNEI1 (ENAdd a b) vts s = compileNEI1 a vts s ++ compileNEI1 b (VOperand : vts) s ++ [I1Add]
+compileNEI1 (ENMul a b) vts s = compileNEI1 a vts s ++ compileNEI1 b (VOperand : vts) s ++ [I1Mul]
+compileNEI1 (ENVar i) vts s = [I1Var $ mapsEnvar2I1var vts i]
+compileNEI1 (ENLet val expr) vts s = compileNEI1 val vts s ++ compileNEI1 expr (VVar : vts) s ++ [I1Swap, I1Pop]
+
+test8 = evalInstr1 (compileNEI1 exprNamelessInstance [] []) []
+
 main :: IO ()
 main = do
     print test1
@@ -99,3 +141,5 @@ main = do
     print test4
     print test5
     print test6
+    print test7
+    print test8
